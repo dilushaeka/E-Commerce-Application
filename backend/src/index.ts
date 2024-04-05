d0d806f8441a740ce9f5dcc9814026b86c8d934b
dilushaeka <dilushaeka99@gmail.com>
2024-04-05 17:26:19 +0530

    Created api for Get All Products with User Name

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -51,47 +51,6 @@ db.on('open', () => {
 
 
 
-//================================= get All Products ====================================================
-
-
-//================================= get All Products With User Name====================================================
-
-app.get('/products/:username',async (req:express.Request,res:express.Response)=>{
-    try {
-
-        let username:string = req.params.username;
-
-
-        let req_query:any=req.query;
-        let size=req_query.size;
-        let page=req_query.page;
-
-
-        let documentCount= await productModel.countDocuments();
-        let pageCount= Math.ceil(documentCount/size);
-        let nextPages=Math.ceil(pageCount-page);
-
-
-        let user:any= await  UserModel.findOne({username: username});
-
-        if (!user){
-            res.status(404).send(
-                new CustomResponse(404,"User Not Found")
-            )
-        }else {
-            let articles=await ProductModel.find({user: user._id}).limit(size).skip(size * (page - 1))
-            res.status(200).send(
-                new CustomResponse(200,"Products Are Found Successfully with user name", articles,pageCount,nextPages)
-            )
-        }
-        // res.status(200).send(
-        //     new CustomResponse(200,"Products Are Found Successfully")
-        // )65d4ac0cc71b08b08ce269d9
-
-    }catch (error){
-        res.status(100).send("Error"+error);
-    }
-})
 
 
 //================================= get logged  all my products============================================
--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -49,6 +49,9 @@ router.post('/products/save',verifyToken,async (req: express.Request, res: any) 
     }
 });
 
+
+//================================= get All Products ====================================================
+
 router.get('/products/all', async (req: express.Request, res: express.Response) => {
 
     try {
@@ -71,3 +74,44 @@ router.get('/products/all', async (req: express.Request, res: express.Response) 
         res.status(404).send("Error :" + error);
     }
 });
+
+
+//================================= get All Products With User Name====================================================
+
+router.get('/products/:username',async (req:express.Request,res:express.Response)=>{
+    try {
+
+        let username:string = req.params.username;
+
+
+        let req_query:any=req.query;
+        let size=req_query.size;
+        let page=req_query.page;
+
+
+        let documentCount= await productModel.countDocuments();
+        let pageCount= Math.ceil(documentCount/size);
+        let nextPages=Math.ceil(pageCount-page);
+
+
+        let user:any= await  UserModel.findOne({username: username});
+
+        if (!user){
+            res.status(404).send(
+                new CustomResponse(404,"User Not Found")
+            )
+        }else {
+            let articles=await ProductModel.find({user: user._id}).limit(size).skip(size * (page - 1))
+            res.status(200).send(
+                new CustomResponse(200,"Products Are Found Successfully with user name", articles,pageCount,nextPages)
+            )
+        }
+        // res.status(200).send(
+        //     new CustomResponse(200,"Products Are Found Successfully")
+        // )65d4ac0cc71b08b08ce269d9
+
+    }catch (error){
+        res.status(100).send("Error"+error);
+    }
+})
+
