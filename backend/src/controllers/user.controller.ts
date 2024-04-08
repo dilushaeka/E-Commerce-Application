1c7e0cf9313f05eb5655693949f566f14f868d47
dilushaeka <dilushaeka99@gmail.com>
2024-04-08 10:02:05 +0530

    Implement Auth User Controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import express from "express";
 import UserModel from "../models/user.model";
 import CustomResponse from "../dtos/custom.response";
 import * as SchemaTypes from "../types/SchemaTypes";
+import jwt, {Secret} from "jsonwebtoken";
 
 //Get All user
 export const getAllUser=async (req: express.Request, res: express.Response) => {
@@ -49,3 +50,49 @@ export const createUser=async (req: express.Request, res: express.Response) => {
 
 }
 
+//User Auth
+
+export const authUser=async (req: express.Request, res: express.Response) => {
+    try {
+        let request_body = req.body
+
+        let user:SchemaTypes.Iuser|null = await UserModel.findOne({email: request_body.email});
+        if (user) {
+
+            if (user.password === request_body.password) {
+
+                //gen token
+                user.password=""
+
+                const expiresIn='1y'
+
+                jwt.sign({user},process.env.SECRET as Secret,{expiresIn},(err:any,token:any)=>{
+                    if (err){
+                        res.status(100).send(
+                            new CustomResponse(100,"Something Went Wrong")
+                        )
+                    }else {
+                        let  res_body ={
+                            user:user,
+                            accessToken:token
+                        }
+                        res.status(200).send(
+                            new CustomResponse(200, "Access", res_body)
+                        )
+                    }
+                })
+            } else {
+                res.status(401).send(
+                    new CustomResponse(401, "Invalid credentials")
+                );
+            }
+        } else {
+            res.status(404).send(
+                new CustomResponse(404, "user not found")
+            );
+        }
+
+    } catch (error) {
+        res.status(100).send("Error" + error);
+    }
+}
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -18,54 +18,11 @@ import * as UserController from "../controllers/user.controller";
 router.get('/all',UserController.getAllUser );
 
 //=================================create new user====================================================
-router.post('/register', );
+router.post('/register', UserController.createUser);
 
 //================================= User Auth ========================================================
 
 
-router.post('/auth', async (req: express.Request, res: express.Response) => {
-    try {
-        let request_body = req.body
-
-        let user:SchemaTypes.Iuser|null = await UserModel.findOne({email: request_body.email});
-        if (user) {
-
-            if (user.password === request_body.password) {
-
-                //gen token
-                user.password=""
-
-                const expiresIn='1y'
-
-                jwt.sign({user},process.env.SECRET as Secret,{expiresIn},(err:any,token:any)=>{
-                    if (err){
-                        res.status(100).send(
-                            new CustomResponse(100,"Something Went Wrong")
-                        )
-                    }else {
-                        let  res_body ={
-                            user:user,
-                            accessToken:token
-                        }
-                        res.status(200).send(
-                            new CustomResponse(200, "Access", res_body)
-                        )
-                    }
-                })
-            } else {
-                res.status(401).send(
-                    new CustomResponse(401, "Invalid credentials")
-                );
-            }
-        } else {
-            res.status(404).send(
-                new CustomResponse(404, "user not found")
-            );
-        }
-
-    } catch (error) {
-        res.status(100).send("Error" + error);
-    }
-});
+router.post('/auth', UserController.authUser);
 
 export default router;
