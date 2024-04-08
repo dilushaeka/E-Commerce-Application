fc08c989bf476f8ee62fd1c7842616888de9320e
dilushaeka <dilushaeka99@gmail.com>
2024-04-08 10:34:02 +0530

    creatd Delete Product Controller

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -33,51 +33,11 @@ router.get('/all', productController.getAllProducts);
 router.get('/:username',productController.getAllProductsWithUserName)
 
 //================================= get logger  all products ============================================
-router.get('/get/my',verifyToken ,getLoggerAllProducts)
+router.get('/get/my',verifyToken ,productController.getLoggerAllProducts)
 
 
 //================================= Update products============================================
-router.put('/',verifyToken,async (req:express.Request,res:any)=>{
-    try {
-
-        let req_body:any=req.body;
-
-        let user_id=res.tokenData.user._id;
-
-        let product= await ProductModel.find({_id:req_body.id,user:user_id})
-
-        console.log(product)
-        if (product){
-            await productModel.findOneAndUpdate({_id:req_body.id},{
-                name: req_body.name,
-                // id: req_body.id
-                category: req_body.category,
-                description: req_body.description,
-                originalPrice: req_body.originalPrice,
-                // image: req_body.image,
-                stock: req_body.stock,
-                created: req_body.created,
-                user: req_body.user
-            })
-                .then( r=>{
-                    res.status(200).send(
-                        new CustomResponse(200,"Product Updated Successfully")
-                    )
-                }).catch( error =>{
-                    console.log(error)
-                    res.status(100).send(
-                        new CustomResponse(100,"Something Went wrong")
-                    )
-                })
-        }else {
-            res.status(401).send(
-                new CustomResponse(401,"Access Denied")
-            )
-        }
-    }catch (error){
-        res.status(100).send("error"+error)
-    }
-})
+router.put('/',verifyToken,productController.updateProduct)
 
 //================================= Delete products============================================
 router.delete('/:id',verifyToken,async (req:express.Request,res:any)=>{
